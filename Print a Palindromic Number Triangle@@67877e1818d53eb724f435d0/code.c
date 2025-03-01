b9df9b0c0bb749e51d840905f444b0e2c7a53606
Aastha Sharma <aastha.sharma6767@gmail.com>
2025-03-02 01:35:21 +0530

    Solution to Print a Palindromic Number Triangle

--- Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
+++ Print a Palindromic Number Triangle@@67877e1818d53eb724f435d0/code.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
+
+// Function to compute binomial coefficient (nCr)
+int binomialCoeff(int n, int k) {
+    int res = 1;
+    if (k > n - k)
+        k = n - k;
+    for (int i = 0; i < k; i++) {
+        res = res * (n - i) / (i + 1);
+    }
+    return res;
+}
+
 int main() {
     int n;
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i; j++) {
-            printf("%d", j);
-            if (j < i) {
-                printf(" ");
-            }
+
+    for (int i = 0; i < n; i++) {
+        // Print leading spaces
+        for (int j = 0; j < n - i - 1; j++) {
+            printf(" ");
         }
-        for (int j = i - 1; j >= 1; j--) {
-            printf("%d", j);
+        // Print Pascal's Triangle values
+        for (int j = 0; j <= i; j++) {
+            printf("%d ", binomialCoeff(i, j));
         }
         printf("\n");
     }
+
     return 0;
 }
